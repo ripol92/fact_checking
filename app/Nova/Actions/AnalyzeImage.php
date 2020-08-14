a53b5685111ac41202037751880c0318cafdcbe9
komron-m <miralizoda.komron@gmail.com>
2020-08-14 16:05:04 +0500

    Add file size validation

--- app/Nova/Actions/AnalyzeImage.php
+++ app/Nova/Actions/AnalyzeImage.php
@@ -40,7 +40,7 @@ class AnalyzeImage extends DetachedAction
     {
         return [
             Image::make("Input Image", "input_img")
-                ->rules(["required", "mimes:jpg,jpeg,png"])
+                ->rules(["required", "file", "max:2048", "mimes:jpg,jpeg,png"])
         ];
     }
 }
