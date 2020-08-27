c2f4e319f69530ed349259fd86b32260d718a698
Furkat Subkhonkulov <subhonkulov2@gmail.com>
2020-08-27 18:26:13 +0500

    added image for rss get

--- app/Services/RssFeedService.php
+++ app/Services/RssFeedService.php
@@ -7,10 +7,8 @@ use App\Models\RssFeedResource;
 use Feed;
 use Illuminate\Support\Facades\Log;
 
-class RssFeedService
-{
-    public static function allRssFeedNews($lang = null)
-    {
+class RssFeedService {
+    public static function allRssFeedNews($lang = null) {
         $news = [];
 
         $languages = $lang ? Language::where('name', $lang) : Language::all();
@@ -26,13 +24,23 @@ class RssFeedService
                 continue;
             }
             foreach ($rss->item as $item) {
+                $img = "";
+                $firstFeed = reset($item->{'content:encoded'});
+                if ($firstFeed) {
+                    $all = [];
+                    preg_match("/(?:http|https|ftp):\/\/\S+\.(?:jpg|jpeg|png)/", $firstFeed, $all);
+                    if (sizeof($all) > 0) {
+                        $img = reset($all);
+                    }
+                }
                 $news[] = [
                     'title' => strip_tags($item->title),
                     'description' => strip_tags($item->description),
                     'link' => strip_tags($item->link),
                     'date' => date("Y-m-d h:i:sa", strip_tags($item->timestamp)),
                     'lang' => $rssFeedResource->language->name,
-//                'html_encoded' => $item->{'content:encoded'}
+//                    'html_encoded' => $item->{'content:encoded'}
+                    "img" => $img
                 ];
             }
         }
